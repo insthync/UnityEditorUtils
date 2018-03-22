302666982a2b387cc8c60ee929a42c71bc625577
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-03-22 12:24:09 +0700

    Add components

---
+++ Scripts/UnityLayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class UnityLayer
+{
+    [SerializeField]
+    private int layerIndex = 0;
+    public int LayerIndex
+    {
+        get { return layerIndex; }
+    }
+
+    public void Set(int _layerIndex)
+    {
+        if (_layerIndex > 0 && _layerIndex < 32)
+            layerIndex = _layerIndex;
+    }
+
+    public int Mask
+    {
+        get { return 1 << layerIndex; }
+    }
+}
---
+++ Scripts/UnityScene.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class UnityScene
+{
+    [SerializeField]
+    private Object sceneAsset;
+    [SerializeField]
+    private string sceneName = "";
+
+    public string SceneName
+    {
+        get { return sceneName; }
+    }
+
+    // makes it work with the existing Unity methods (LoadLevel/LoadScene)
+    public static implicit operator string(UnityScene unityScene)
+    {
+        return unityScene.SceneName;
+    }
+
+    public bool IsSet()
+    {
+        return !string.IsNullOrEmpty(sceneName);
+    }
+}
