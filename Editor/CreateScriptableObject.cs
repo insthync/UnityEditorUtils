029f702715abc6b5d7cbf9cd366c0960554f8247
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-04-04 16:47:41 +0700

    Add scriptable object utility

---
+++ Editor/CreateScriptableObject.cs
@@ -0,0 +1,48 @@
+﻿// This class can be used to create any ScriptableObject type via Reflection.
+// To use it, simply right click in the Project Area and select Create ->
+// ScriptableObject.
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.ProjectWindowCallback;
+using UnityEngine;
+
+class EndNameEdit : EndNameEditAction {
+    public override void Action (int instanceId, string pathName, string resourceFile) {
+        AssetDatabase.CreateAsset(EditorUtility.InstanceIDToObject(instanceId), AssetDatabase.GenerateUniqueAssetPath(pathName));
+    }
+}
+
+public class CreateScriptableObject : EditorWindow {
+    static List<Type> FindTypes() {
+        // get project assembly
+        var asm = Assembly.Load(new AssemblyName("Assembly-CSharp"));
+
+        // filter out all the ScriptableObject types
+        var types = new List<Type>();
+        foreach (Type t in asm.GetTypes())
+            if (t.IsSubclassOf(typeof(ScriptableObject)) && !t.IsAbstract)
+                types.Add(t);
+        
+        return types;
+    }
+    
+    void OnGUI() {
+        GUILayout.Label("Select the type to create:");
+        foreach (Type t in FindTypes()) {
+            if (GUILayout.Button(t.FullName)) {
+                // create the asset, select it, allow renaming, close
+                var asset = ScriptableObject.CreateInstance(t);
+                ProjectWindowUtil.StartNameEditingIfProjectWindowExists(asset.GetInstanceID(), ScriptableObject.CreateInstance<EndNameEdit>(), t.FullName + ".asset", AssetPreview.GetMiniThumbnail(asset), null);
+                Close();
+            }
+        }
+    }
+
+    [MenuItem("Assets/Create/ScriptableObject")]
+    public static void ShowWindow() {
+        var win = EditorWindow.GetWindow<CreateScriptableObject>(true, "Create ScriptableObject");
+        win.ShowPopup();
+    }
+}
---
+++ Scripts/ScriptableObjectUtility.cs
@@ -0,0 +1,37 @@
+﻿#if UNITY_EDITOR
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public static class ScriptableObjectUtility
+{
+    /// <summary>
+    //	This makes it easy to create, name and place unique new ScriptableObject asset files.
+    /// </summary>
+    public static T CreateAsset<T>(string fileName) where T : ScriptableObject
+    {
+        T asset = ScriptableObject.CreateInstance<T>();
+
+        string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        if (path == "")
+        {
+            path = "Assets";
+        }
+        else if (Path.GetExtension(path) != "")
+        {
+            path = path.Replace(Path.GetFileName(AssetDatabase.GetAssetPath(Selection.activeObject)), "");
+        }
+
+        string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + "/" + fileName + ".asset");
+
+        AssetDatabase.CreateAsset(asset, assetPathAndName);
+
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = asset;
+
+        return asset;
+    }
+}
+#endif
