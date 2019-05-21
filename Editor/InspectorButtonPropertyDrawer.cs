28e9fe067c98d71f56b1ccb9e6c94fe317ce36e2
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-05-21 23:41:56 +0700

    Add inspector button

---
+++ Editor/InspectorButtonPropertyDrawer.cs
@@ -0,0 +1,30 @@
+﻿// These codes are come from: https://raw.githubusercontent.com/zaikman/UnityPublic/master/InspectorButton.cs
+
+using System.Reflection;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(InspectorButtonAttribute))]
+public class InspectorButtonPropertyDrawer : PropertyDrawer
+{
+    private MethodInfo _eventMethodInfo = null;
+
+    public override void OnGUI(Rect position, SerializedProperty prop, GUIContent label)
+    {
+        InspectorButtonAttribute inspectorButtonAttribute = (InspectorButtonAttribute)attribute;
+        Rect buttonRect = new Rect(position.x, position.y, position.width, position.height);
+        if (GUI.Button(buttonRect, label.text))
+        {
+            System.Type eventOwnerType = prop.serializedObject.targetObject.GetType();
+            string eventName = inspectorButtonAttribute.MethodName;
+
+            if (_eventMethodInfo == null)
+                _eventMethodInfo = eventOwnerType.GetMethod(eventName, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+
+            if (_eventMethodInfo != null)
+                _eventMethodInfo.Invoke(prop.serializedObject.targetObject, null);
+            else
+                Debug.LogWarning(string.Format("InspectorButton: Unable to find method {0} in {1}", eventName, eventOwnerType));
+        }
+    }
+}
---
+++ Scripts/InspectorButtonAttribute.cs
@@ -0,0 +1,20 @@
+﻿// These codes are come from: https://raw.githubusercontent.com/zaikman/UnityPublic/master/InspectorButton.cs
+
+using UnityEngine;
+
+/// <summary>
+/// This attribute can only be applied to fields because its
+/// associated PropertyDrawer only operates on fields (either
+/// public or tagged with the [SerializeField] attribute) in
+/// the target MonoBehaviour.
+/// </summary>
+[System.AttributeUsage(System.AttributeTargets.Field)]
+public class InspectorButtonAttribute : PropertyAttribute
+{
+    public readonly string MethodName;
+
+    public InspectorButtonAttribute(string MethodName)
+    {
+        this.MethodName = MethodName;
+    }
+}
