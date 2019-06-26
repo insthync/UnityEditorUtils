01776294c52d87137febcd49286288697ca6ec88
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-06-26 10:08:28 +0700

    Make it supports multiple assembly

--- Editor/CreateScriptableObject.cs
+++ Editor/CreateScriptableObject.cs
@@ -8,41 +8,55 @@ using UnityEditor;
 using UnityEditor.ProjectWindowCallback;
 using UnityEngine;
 
-class EndNameEdit : EndNameEditAction {
-    public override void Action (int instanceId, string pathName, string resourceFile) {
+class EndNameEdit : EndNameEditAction
+{
+    public override void Action(int instanceId, string pathName, string resourceFile)
+    {
         AssetDatabase.CreateAsset(EditorUtility.InstanceIDToObject(instanceId), AssetDatabase.GenerateUniqueAssetPath(pathName));
     }
 }
 
-public class CreateScriptableObject : EditorWindow {
-    static List<Type> FindTypes() {
+public class CreateScriptableObject : EditorWindow
+{
+    public string[] assemblyNames = new string[] { "Assembly-CSharp" };
+
+    static List<Type> FindTypes(string name)
+    {
         // get project assembly
-        var asm = Assembly.Load(new AssemblyName("Assembly-CSharp"));
+        var asm = Assembly.Load(new AssemblyName(name));
 
         // filter out all the ScriptableObject types
         var types = new List<Type>();
         foreach (Type t in asm.GetTypes())
             if (t.IsSubclassOf(typeof(ScriptableObject)) && !t.IsAbstract)
                 types.Add(t);
-        
+
         return types;
     }
-    
-    void OnGUI() {
+
+    void OnGUI()
+    {
         GUILayout.Label("Select the type to create:");
-        foreach (Type t in FindTypes()) {
-            if (GUILayout.Button(t.FullName)) {
-                // create the asset, select it, allow renaming, close
-                var asset = ScriptableObject.CreateInstance(t);
-                ProjectWindowUtil.StartNameEditingIfProjectWindowExists(asset.GetInstanceID(), ScriptableObject.CreateInstance<EndNameEdit>(), t.FullName + ".asset", AssetPreview.GetMiniThumbnail(asset), null);
-                Close();
+        foreach (var assemblyName in assemblyNames)
+        {
+            foreach (Type t in FindTypes(assemblyName))
+            {
+                if (GUILayout.Button(t.FullName))
+                {
+                    // create the asset, select it, allow renaming, close
+                    var asset = ScriptableObject.CreateInstance(t);
+                    ProjectWindowUtil.StartNameEditingIfProjectWindowExists(asset.GetInstanceID(), ScriptableObject.CreateInstance<EndNameEdit>(), t.FullName + ".asset", AssetPreview.GetMiniThumbnail(asset), null);
+                    Close();
+                }
             }
         }
     }
 
     [MenuItem("Assets/Create/ScriptableObject")]
-    public static void ShowWindow() {
+    public static void ShowWindow()
+    {
         var win = EditorWindow.GetWindow<CreateScriptableObject>(true, "Create ScriptableObject");
+        win.assemblyNames = new string[] { "Assembly-CSharp" };
         win.ShowPopup();
     }
 }
