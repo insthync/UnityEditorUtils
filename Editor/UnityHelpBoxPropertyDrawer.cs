5d6db2e061f4dad651b11cd55ea681318556efd3
Insthync <ittipon.bay@gmail.com>
2020-11-12 08:33:16 +0700

    Add help box

---
+++ Editor/UnityHelpBoxPropertyDrawer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(UnityHelpBox))]
+public class UnityHelpBoxPropertyDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        UnityHelpBox target = fieldInfo.GetValue(property.serializedObject.targetObject) as UnityHelpBox;
+        EditorGUI.BeginProperty(position, label, property);
+        EditorGUI.HelpBox(position, target.text, (MessageType)target.type);
+        EditorGUI.EndProperty();
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        UnityHelpBox target = fieldInfo.GetValue(property.serializedObject.targetObject) as UnityHelpBox;
+        return target.height;
+    }
+}
---
+++ Scripts/UnityHelpBox.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+[Serializable]
+public class UnityHelpBox
+{
+    public enum Type
+    {
+        None,
+        Info,
+        Warning,
+        Error
+    }
+
+    [NonSerialized]
+    public string text;
+    [NonSerialized]
+    public float height;
+    [NonSerialized]
+    public Type type;
+
+    public UnityHelpBox(string text, float height, Type type = Type.Info)
+    {
+        this.text = text;
+        this.height = height;
+        this.type = type;
+    }
+
+    public UnityHelpBox(string text, Type type = Type.Info)
+    {
+        this.text = text;
+        height = 40;
+        this.type = type;
+    }
+
+    public UnityHelpBox()
+    {
+        height = 40;
+        text = "";
+        type = Type.Info;
+    }
+}
