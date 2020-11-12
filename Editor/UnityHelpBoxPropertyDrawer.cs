0258353bd4402b643205e6c6134f2ea72b46f8a4
Insthync <ittipon.bay@gmail.com>
2020-11-12 09:41:09 +0700

    Improve help box to make it able to find height

--- Editor/UnityHelpBoxPropertyDrawer.cs
+++ Editor/UnityHelpBoxPropertyDrawer.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 [CustomPropertyDrawer(typeof(UnityHelpBox))]
 public class UnityHelpBoxPropertyDrawer : PropertyDrawer
 {
+    public const float ExtraSpace = 12f;
+    public const float MinHeight = 40f + ExtraSpace;
+
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
         UnityHelpBox target = fieldInfo.GetValue(property.serializedObject.targetObject) as UnityHelpBox;
@@ -15,6 +18,9 @@ public class UnityHelpBoxPropertyDrawer : PropertyDrawer
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
         UnityHelpBox target = fieldInfo.GetValue(property.serializedObject.targetObject) as UnityHelpBox;
-        return target.height;
+        GUIContent content = new GUIContent(target.text);
+        GUIStyle style = GUI.skin.GetStyle("helpbox");
+        float height = style.CalcHeight(content, EditorGUIUtility.currentViewWidth) + ExtraSpace;
+        return height > MinHeight ? height : MinHeight;
     }
 }
--- Scripts/UnityHelpBox.cs
+++ Scripts/UnityHelpBox.cs
@@ -13,29 +13,13 @@ public class UnityHelpBox
 
     [NonSerialized]
     public string text;
-    [NonSerialized]
-    public float height;
+
     [NonSerialized]
     public Type type;
-
-    public UnityHelpBox(string text, float height, Type type = Type.Info)
-    {
-        this.text = text;
-        this.height = height;
-        this.type = type;
-    }
 
     public UnityHelpBox(string text, Type type = Type.Info)
     {
         this.text = text;
-        height = 40;
         this.type = type;
-    }
-
-    public UnityHelpBox()
-    {
-        height = 40;
-        text = "";
-        type = Type.Info;
     }
 }
