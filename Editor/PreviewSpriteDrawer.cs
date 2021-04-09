594510316d90830e0b7fb44d9d7fc9da583e23bc
Insthync <ittipon.bay@gmail.com>
2021-04-10 05:17:52 +0700

    Add preview sprite attribute

---
+++ Editor/PreviewSpriteDrawer.cs
@@ -0,0 +1,62 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(PreviewSpriteAttribute))]
+public class PreviewSpriteDrawer : PropertyDrawer
+{
+    protected PreviewSpriteAttribute Attribute
+    {
+        get { return (PreviewSpriteAttribute)attribute; }
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        if (property.propertyType == SerializedPropertyType.ObjectReference &&
+            (property.objectReferenceValue as Sprite) != null)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true) + Attribute.height + 10;
+        }
+        return EditorGUI.GetPropertyHeight(property, label, true);
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        //Draw the normal property field
+        EditorGUI.PropertyField(position, property, label, true);
+
+        if (property.propertyType == SerializedPropertyType.ObjectReference)
+        {
+            var sprite = property.objectReferenceValue as Sprite;
+            if (sprite != null)
+            {
+                position.y += EditorGUI.GetPropertyHeight(property, label, true) + 5;
+                position.height = Attribute.height;
+
+                DrawTexturePreview(position, sprite);
+            }
+        }
+    }
+
+    private void DrawTexturePreview(Rect position, Sprite sprite)
+    {
+        Vector2 fullSize = new Vector2(sprite.texture.width, sprite.texture.height);
+        Vector2 size = new Vector2(sprite.textureRect.width, sprite.textureRect.height);
+
+        Rect coords = sprite.textureRect;
+        coords.x /= fullSize.x;
+        coords.width /= fullSize.x;
+        coords.y /= fullSize.y;
+        coords.height /= fullSize.y;
+
+        Vector2 ratio;
+        ratio.x = position.width / size.x;
+        ratio.y = position.height / size.y;
+        float minRatio = Mathf.Min(ratio.x, ratio.y);
+
+        position.width = size.x * minRatio;
+        position.height = size.y * minRatio;
+        position.center = new Vector2((position.width * 0.5f) + 5, position.center.y);
+
+        GUI.DrawTextureWithTexCoords(position, sprite.texture, coords);
+    }
+}
---
+++ Scripts/PreviewSpriteAttribute.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class PreviewSpriteAttribute : PropertyAttribute
+{
+    public int height { get; private set; }
+    public PreviewSpriteAttribute(int height = 100)
+    {
+        this.height = height;
+    }
+}
