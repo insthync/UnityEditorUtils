7faefb9db32322f1c30d93ab7eb89b8341211b33
Insthync <ittipon.bay@gmail.com>
2021-07-14 14:10:13 +0700

    Improve InspectorButton to be able to called by interiting classes

--- Editor/InspectorButtonPropertyDrawer.cs
+++ Editor/InspectorButtonPropertyDrawer.cs
@@ -3,11 +3,12 @@
 using System.Reflection;
 using UnityEngine;
 using UnityEditor;
+using System;
 
 [CustomPropertyDrawer(typeof(InspectorButtonAttribute))]
 public class InspectorButtonPropertyDrawer : PropertyDrawer
 {
-    private MethodInfo _eventMethodInfo = null;
+    private MethodInfo eventMethodInfo = null;
 
     public override void OnGUI(Rect position, SerializedProperty prop, GUIContent label)
     {
@@ -15,14 +16,17 @@ public class InspectorButtonPropertyDrawer : PropertyDrawer
         Rect buttonRect = new Rect(position.x, position.y, position.width, position.height);
         if (GUI.Button(buttonRect, label.text))
         {
-            System.Type eventOwnerType = prop.serializedObject.targetObject.GetType();
+            Type eventOwnerType = prop.serializedObject.targetObject.GetType();
             string eventName = inspectorButtonAttribute.MethodName;
 
-            if (_eventMethodInfo == null)
-                _eventMethodInfo = eventOwnerType.GetMethod(eventName, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+            do
+            {
+                eventMethodInfo = eventOwnerType.GetMethod(eventName, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+                eventOwnerType = eventOwnerType.BaseType;
+            } while (eventMethodInfo == null);
 
-            if (_eventMethodInfo != null)
-                _eventMethodInfo.Invoke(prop.serializedObject.targetObject, null);
+            if (eventMethodInfo != null)
+                eventMethodInfo.Invoke(prop.serializedObject.targetObject, null);
             else
                 Debug.LogWarning(string.Format("InspectorButton: Unable to find method {0} in {1}", eventName, eventOwnerType));
         }
