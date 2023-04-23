b387608e26af7cd9b4ca93ade3d50b59769c5cc9
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-04-24 02:14:42 +0700

    improve: delete unused namespace

--- Editor/BaseShowConditionalPropertyDrawer.cs
+++ Editor/BaseShowConditionalPropertyDrawer.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
 
 public abstract class BaseShowConditionalPropertyDrawer<T> : PropertyDrawer where T : BaseShowConditionalAttribute
